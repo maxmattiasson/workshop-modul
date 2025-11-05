738db56c3fcf9168a7cdbb32666456b074ace5ae
agnes.varnlund@hotmail.com <agnes.varnlund@hotmail.com>
2025-11-05 10:32:40 +0100

    hej

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 import { convert as convertTemp } from "./convertTemp.js";
 import { convert as convertPounds } from "./moduleConvertPounds.js";
+import { convert as convertCm } from "./convertMeasure.js";
 
 console.log(convertTemp(20));
 convertPounds(150);
