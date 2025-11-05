e9991107dfbb93a4c40de1dd2d47477211aa5128
Max Mattiasson <max@mattiasson.eu>
2025-11-05 11:42:30 +0100

    lol

--- convertTemp.js
+++ convertTemp.js
@@ -1,3 +1,4 @@
-export function convert(celsius) {
-  return (celsius * 9) / 5 + 32;
+export function convert(celsius, unit) {
+  if (unit === "C") return (celsius * 9) / 5 + 32;
+  if (unit === "F") return ((celsius - 32) * 5) / 9;
 }
--- script.js
+++ script.js
@@ -4,20 +4,27 @@ import { convert as convertCm } from "./convertMeasure.js";
 
 document.querySelector("#celcius-btn").addEventListener("click", () => {
   const input = document.querySelector("#input-celsius").value;
+
+  const unit = document.querySelector("#cels").checked ? "C" : "F";
+  let toUnit = "C";
+  if (unit === "C") {
+    toUnit = "F";
+  }
+
   document.querySelector(
     "#temp-svar"
-  ).textContent = `${input} grader celcius motsvarar ${convertTemp(
-    input
-  )} grader fahrenheit`;
+  ).textContent = `${input} grader ${unit} motsvarar ${convertTemp(
+    input,
+    unit
+  )} grader av ${toUnit} `;
 });
 
 const input = document.getElementById("cmInput");
 const button = document.getElementById("measure-btn");
 const result = document.getElementById("resultM");
 
-
 button.addEventListener("click", () => {
-  const cm = (input.value);
+  const cm = input.value;
   result.innerText = `${convertCm(cm)} m`;
 });
 
