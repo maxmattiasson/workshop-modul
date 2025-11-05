ff9d69a47737d7319538cf99048c8ac9e3233917
Max Mattiasson <max@mattiasson.eu>
2025-11-05 10:53:04 +0100

    test cels

--- script.js
+++ script.js
@@ -2,5 +2,13 @@ import { convert as convertTemp } from "./convertTemp.js";
 import { convert as convertPounds } from "./moduleConvertPounds.js";
 import { convert as convertCm } from "./convertMeasure.js";
 
+document.querySelector("#celcius-btn").addEventListener("click", () => {
+  const input = document.querySelector("#input-celsius").value;
+  document.querySelector(
+    "#temp-svar"
+  ).textContent = `${input} grader celcius motsvarar ${convertTemp(
+    input
+  )} grader fahrenheit`;
+});
 console.log(convertTemp(20));
 convertPounds(150);
